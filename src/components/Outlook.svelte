4d456b32d3d16730f9c2f3d83e7e1629765b7881
Alex Evangelou <35777043+AJevangelou@users.noreply.github.com>
2024-06-12 20:22:35 +0300

    1. Fully working Outlook application 2. Created custom alert view

--- src/components/Outlook.svelte
+++ src/components/Outlook.svelte
@@ -1,15 +1,27 @@
 <script>
-  import { showOutlook, hideOutlook } from "../stores";
+  import { showOutlook, hideOutlook, show } from "../stores";
   import { draggable } from "@neodrag/svelte";
   import { resize } from "@svelte-put/resize";
   import outlook from "../assets/outlook.png";
   import window from "../assets/window.svg";
   import logo from "../assets/win_xp_logo.webp";
+  import send from "../assets/send.png";
+  import emailjs from "emailjs-com";
+  import AboutMe from "./AboutMe.svelte";
   let position = { x: 0, y: 0 };
   let storedWidth;
   let storedHeight;
   let isMaximized = false;
   let enabled = true;
+  let showAlert = false;
+  let error_message = "";
+  let form = {
+    from_name: "",
+    message: "",
+    subject: "",
+  };
+  let statusMessage = "";
+
   function handleMaximizeClick() {
     if (!isMaximized) {
       position.x = 0;
@@ -29,8 +41,47 @@
     showOutlook.update((currentValue) => false);
     hideOutlook.update((currentValue) => false);
   }
-  function handleSend() {
-    console.log("Email sent");
+  function handleSend(e) {
+    e.preventDefault();
+    console.log("Pressed send");
+
+    emailjs
+      .send("service_67mjpvb", "template_6nxurl7", form, "iLLHHq_QztgEZE-pa")
+      .then(
+        (response) => {
+          if (
+            form.from_name !== "" &&
+            form.message !== "" &&
+            form.subject !== ""
+          ) {
+            console.log("SUCCESS!", response.status, response.text);
+            statusMessage = "Email successfully sent!";
+            form.from_name = "";
+            form.subject = "";
+            form.message = "";
+            showAlert = !showAlert;
+          } else if (form.from_name === "") {
+            showAlert = true;
+            error_message = "Please fill in your email";
+          } else if (form.subject === "") {
+            showAlert = true;
+            error_message = "Please fill in the subject";
+          } else if (form.message === "") {
+            showAlert = true;
+            error_message = "Please fill in your message";
+          } else {
+            showAlert = true;
+            error_message = "An unexpected error occured";
+          }
+        },
+        (err) => {
+          console.error("FAILED...", err);
+          statusMessage = "Failed to send email. Please try again later.";
+        }
+      );
+  }
+  function closeAlert() {
+    showAlert = false;
   }
 </script>
 
@@ -123,8 +174,13 @@
       class="w-full h-10 bg-[#EFECDE] flex flex-row justify-between border-b-[1px] border-[#E3DECB]"
     >
       <button
-        class="ml-2 px-2 text-xs font-light hover:bg-[#235CD9] hover:text-white"
-        on:click={handleSend}>Send</button
+        class=" px-4 text-xs font-light ml-2 hover:bg-[#3168D5] hover:text-white items-center justify-center"
+        on:click={handleSend}
+        ><img
+          src={send}
+          alt="Send button"
+          class="h-6 w-6 ml-[2px]"
+        />Send</button
       >
     </div>
     <!-- From, To and Subject -->
@@ -134,23 +190,23 @@
       <label for="from_address" class="w-14">From: </label>
       <input
         type="text"
+        placeholder="Enter your email"
         id="from_address"
         name="from_address"
         class="ml-4 h-6 pl-2 w-full border-t-[1px] border-l-[1px] border-black focus:outline-none"
+        bind:value={form.from_name}
+        required
       />
     </form>
-    <!-- TODO: Fix this -->
     <form
       class="w-full pl-4 pr-2 text-xs font-light flex flex-row items-center h-8 bg-[#EFECDE] justify-between"
     >
       <label for="from_address" class="w-14">To: </label>
-      <input
-        type="text"
-        id="from_address"
-        name="from_address"
-        placeholder="ioan.alex.evangelou@gmail.com"
-        class="ml-4 h-6 pl-2 w-full border-t-[1px] border-l-[1px] border-black focus:outline-none"
-      />
+      <p
+        class="ml-4 h-6 pl-2 pt-[2px] w-full border-t-[1px] border-l-[1px] border-black focus:outline-none bg-white select-none text-gray-400"
+      >
+        johnalexevangelou@gmail.com
+      </p>
     </form>
     <!-- TODO: Fix this -->
     <form
@@ -162,14 +218,41 @@
         id="from_address"
         name="from_address"
         class="ml-4 h-6 pl-2 w-full border-t-[1px] border-l-[1px] border-black focus:outline-none"
+        bind:value={form.subject}
+        required
       />
     </form>
     <!-- Textarea for email body -->
     <textarea
-      class="w-full h-[156px] border-b-2 border-[#EFECDE] focus:outline-none p-2 resize-none"
+      class="w-full h-[180px] border-b-2 border-[#EFECDE] focus:outline-none p-2 resize-none text-sm"
       placeholder="Type your message here..."
+      bind:value={form.message}
+      required
     ></textarea>
   </div>
+  {#if showAlert}
+    <div
+      class="absolute top-[40%] left-[35%] w-[300px] h-[180px] bg-[#EFECDE] border-4 border-[#235CD9] rounded-t-lg"
+    >
+      <div class="end justify-end pr-[2px]">
+        <button
+          on:click={closeAlert}
+          class="styled-button-red rounded-md flex flex-row items-center justify-center"
+        >
+          <p class="mx-1 text-md font-bold">X</p>
+        </button>
+      </div>
+      <div
+        class="w-full h-[100px] flex flex-col items-center justify-between mt-6"
+      >
+        <p class="font-light text-sm">{error_message}</p>
+        <button
+          class="mt-4 px-6 border-t-[1.3px] border-l-[1.3px] border-b-2 border-r-2 border-black active:bg-[#c6c1ae]"
+          on:click={closeAlert}>OK</button
+        >
+      </div>
+    </div>
+  {/if}
 {/if}
 
 <style>
@@ -182,6 +265,15 @@
     font-size: 0.875rem; /* Equivalent to 14px */
     height: 1.75rem; /* Equivalent to 28px */
     justify-content: space-between;
+  }
+  .end {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    background-image: linear-gradient(to bottom, #235cd9, #2663e0);
+    color: white;
+    font-size: 0.875rem; /* Equivalent to 14px */
+    height: 1.75rem; /* Equivalent to 28px */
   }
   .styled-button-red {
     height: 80%;
