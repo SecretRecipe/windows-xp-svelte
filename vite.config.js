20925f60ff58304348ca82c920020bf2114212ed
Alex Evangelou <35777043+AJevangelou@users.noreply.github.com>
2024-06-28 15:40:21 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/windows-xp-svelte/",
   plugins: [svelte()],
+  base: "/windows-xp-svelte/",
 });
